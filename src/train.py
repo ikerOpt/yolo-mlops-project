ad472e20789046e658115a2a292d0533f3c23d09
KONORXL <ikerrk2322003@gmail.com>
2026-03-07 16:11:53 +0100

    Update config model

--- src/train.py
+++ src/train.py
@@ -4,12 +4,15 @@ if __name__ == "__main__":
     model = YOLO("yolov8n.pt")
     
     model.train(
-        data = "configs/data.yaml",
-        epochs = 5,
-        imgsz = 640,
-        batch = 16,
-        fraction = 0.05,
-        device = "mps",
-        project = "runs",
-        name = "debug-small-mps"
+        data="configs/data.yaml",
+        epochs=50,
+        imgsz=640,
+        batch=8,        # prueba 8 primero
+        workers=0,
+        amp=True,
+        cache="disk",   # usa "ram" si tienes RAM de sobra
+        patience=12,
+        device=0,
+        project="runs_local",
+        name="ppe_rtx2060"
     )
